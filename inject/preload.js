a228bac41a0cde051277f11e39b32f9fb3f39e71
z3db0y <us3r0002@gmail.com>
2024-02-14 16:45:57 +0200

    Give non-bytecode scripts access to require

--- inject/preload.js
+++ inject/preload.js
@@ -1,25 +1,28 @@
 const bytenode = require(__dirname + '/bytenode');
 const { ipcRenderer } = require('electron');
 const preload = ipcRenderer.sendSync('$get-preload$');
-if(preload) require(preload);
+if (preload) require(preload);
 
 (async () => {
     window.addEventListener('message', ({ data }) => {
-        let panicMessage = 'PANIC'.split('').map(x => String.fromCharCode(x.charCodeAt(0) ^ 0x1337)).join('');
-        if(data == panicMessage) ipcRenderer.send('$panic$');
+        let panicMessage = 'PANIC'
+            .split('')
+            .map((x) => String.fromCharCode(x.charCodeAt(0) ^ 0x1337))
+            .join('');
+        if (data == panicMessage) ipcRenderer.send('$panic$');
     });
 
     const cheetInfo = await ipcRenderer.invoke('$get-cheet$');
-    if(!cheetInfo.content || cheetInfo.panic) return;
+    if (!cheetInfo.content || cheetInfo.panic) return;
     const cheetContent = Buffer.from(cheetInfo.content, 'base64');
 
     let url = new URL(location.href);
-    if(!['browserfps.com', 'krunker.io'].includes(url.hostname)) return;
+    if (!['browserfps.com', 'krunker.io'].includes(url.hostname)) return;
 
     window.KRUNKSENSE_TOKEN = cheetInfo.token;
-    if(cheetInfo.bytenode) {
+    if (cheetInfo.bytenode) {
         bytenode.runBytecode(cheetContent)();
     } else {
-        new Function(cheetContent.toString())();
+        new Function('require', cheetContent.toString())(require);
     }
 })();
